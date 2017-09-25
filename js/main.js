d9e8e656015eaceb5a0bd2d3eaa7d8f1594a262b
Do Minh Hai <minhhai3b@gmail.com>
2017-09-25 16:21:04 +0900

    Build on 2017年 9月 25日 月曜日 16:21:03

--- js/main.js
+++ js/main.js
@@ -1,3 +1,9 @@
 $('#language').on('change', function() {
   window.location.pathname = window.location.pathname.replace(/\/(.*?)\//i, '/'+ $(this).val() +'/')
 })
+renderMathInElement(document.body, {delimiters:[
+  {left: "$$", right: "$$", display: true},
+  {left: "\\[", right: "\\]", display: true},
+  {left: "$", right: "$", display: false},
+  {left: "\\(", right: "\\)", display: false}
+ ]})
