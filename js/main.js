cb49ea0243ec32c3011ca923545cfa7f7f9578e4
Hai Do Minh <minhhai3b@gmail.com>
2017-10-31 20:28:48 +0900

    Build on 2017年 10月31日 火曜日 20時28分48秒 JST

--- js/main.js
+++ js/main.js
@@ -1,2 +1,1 @@
-$('#language').on('change',function(){window.location.pathname=window.location.pathname.replace(/\/(.*?)\//i,'/'+$(this).val()+'/')})
-renderMathInElement(document.body,{delimiters:[{left:"$$",right:"$$",display:true},{left:"\\[",right:"\\]",display:true},{left:"$",right:"$",display:false},{left:"\\(",right:"\\)",display:false}]})
+$('#language').on('change',function(){window.location.pathname=window.location.pathname.replace(/\/(.*?)\//i,'/'+$(this).val()+'/');});renderMathInElement(document.body,{delimiters:[{left:"$$",right:"$$",display:true},{left:"\\[",right:"\\]",display:true},{left:"$",right:"$",display:false},{left:"\\(",right:"\\)",display:false}]});
