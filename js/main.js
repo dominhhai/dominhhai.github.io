9c2083a1f678bf102a23bf97b9e1b931609dcd6c
Hai Do Minh <minhhai3b@gmail.com>
2017-10-31 20:44:49 +0900

    Build on 2017年 10月31日 火曜日 20時44分49秒 JST

--- js/main.js
+++ js/main.js
@@ -1,12 +1,1 @@
-// Language Select
-$('#language').on('change', function() {
-  window.location.pathname = window.location.pathname.replace(/\/(.*?)\//i, '/'+ $(this).val() +'/');
-});
-
-// KaTex Math Config
-renderMathInElement(document.body, {delimiters:[
-  { left: "$$"  , right: "$$"  , display: true  },
-  { left: "\\[" , right: "\\]" , display: true  },
-  { left: "$"   , right: "$"   , display: false },
-  { left: "\\(" , right: "\\)" , display: false }
-]});
+$('#language').on('change',function(){window.location.pathname=window.location.pathname.replace(/\/(.*?)\//i,'/'+$(this).val()+'/');});renderMathInElement(document.body,{delimiters:[{left:"$$",right:"$$",display:true},{left:"\\[",right:"\\]",display:true},{left:"$",right:"$",display:false},{left:"\\(",right:"\\)",display:false}]});
