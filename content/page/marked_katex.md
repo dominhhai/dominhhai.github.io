4aa970d0f7582ae317af9aadf34ee3184bbf989b
Hai Do Minh <minhhai3b@gmail.com>
2017-11-21 00:24:20 +0900

    Build on 2017年 11月21日 火曜日 00時24分20秒 JST

---
+++ content/page/marked_katex.md
@@ -0,0 +1,25 @@
+---
+title: "Markdown $\\KaTeX$ Live"
+date: 2017-11-20
+keywords:
+- Markdown
+- KaTex
+- Markdown KaTex Live
+showDate: false
+showSocial: true
+showTags: false
+showPagination: false
+customJS:
+- js/marked.js
+customCSS:
+- css/editor.css
+
+---
+<div id="editor">
+  <div id="code" class="table-cell">
+    <textarea placeholder="Markdown with KaTex" autofocus></textarea>
+  </div>
+  <div id="preview" class="table-cell">
+    <div id="preview-content"></div>
+  </div>
+</div>
--- static/js/main.js
+++ static/js/main.js
@@ -1,12 +1,27 @@
-// Language Select
-$('#language').on('change', function() {
-  window.location.pathname = window.location.pathname.replace(/\/(.*?)\//i, '/'+ $(this).val() +'/');
-});
-
 // KaTex Math Config
-renderMathInElement(document.body, {delimiters:[
+var katexOpts = { delimiters:[
   { left: "$$"  , right: "$$"  , display: true  },
   { left: "\\[" , right: "\\]" , display: true  },
   { left: "$"   , right: "$"   , display: false },
   { left: "\\(" , right: "\\)" , display: false }
-]});
+]};
+// render KaTex Math
+renderMathInElement(document.body, katexOpts);
+
+// Language Select
+$('#language').on('change', function() {
+  window.location.pathname = window.location.pathname.replace(/\/(.*?)\//i, '/'+ this.value +'/');
+});
+
+// Editor
+$('#editor textarea').on('input change', function() {
+  var preview = document.getElementById('preview-content');
+  var rawTxt = this.value.trim();
+  if (rawTxt.length < 1) {
+    return preview.innerHTML = rawTxt;
+  }
+  // to markdown
+  preview.innerHTML = marked(rawTxt);
+  // to KaTeX Math
+  renderMathInElement(preview, katexOpts);
+});
